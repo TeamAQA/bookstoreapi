d97aa2c9b6da3a5e3078bd6ff30f7f9c69f649ff
Bartosz Kita <devnull@testerprogramuje.pl>
2023-01-03 16:56:58 +0100

    Removed fullName from RestAuthor

--- src/main/java/pl/akademiaqa/bos/autors/domain/RestAuthor.java
+++ src/main/java/pl/akademiaqa/bos/autors/domain/RestAuthor.java
@@ -1,10 +1,13 @@
 package pl.akademiaqa.bos.autors.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Value;
 
 @Value
 public class RestAuthor {
     String firstName;
     String lastName;
+
+    @JsonIgnore
     String fullName;
 }
