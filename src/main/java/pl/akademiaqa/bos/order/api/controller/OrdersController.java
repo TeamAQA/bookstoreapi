ae105d2489a01f2ae7c1d0c12ba400b568caabf2
Bartosz Kita <devnull@testerprogramuje.pl>
2023-01-11 12:47:12 +0100

    Added description for BUG 9 - when new order is deleted, books are not returned to the list

--- src/main/java/pl/akademiaqa/bos/order/api/controller/OrdersController.java
+++ src/main/java/pl/akademiaqa/bos/order/api/controller/OrdersController.java
@@ -59,6 +59,7 @@ public class OrdersController {
     @Secured({"ROLE_ADMIN"})
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    // TODO - BUG 9 - Usunięcie zamówienia nie przywraca dostępności książek
     public ResponseEntity deleteOrder(@PathVariable Long id) {
         return orders.deleteById(id);
     }
