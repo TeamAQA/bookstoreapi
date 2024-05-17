ca14beb34ff5c0745b75757b7c50fdcc5de9fb0b
Bartosz <bartosz.kita@gmail.com>
2024-05-17 11:42:59 +0200

    fix PUT

--- src/main/java/pl/akademiaqa/bos/books/api/payload/UpdateBookPayload.java
+++ src/main/java/pl/akademiaqa/bos/books/api/payload/UpdateBookPayload.java
@@ -1,6 +1,9 @@
 package pl.akademiaqa.bos.books.api.payload;
 
 import lombok.Value;
+import pl.akademiaqa.bos.validators.ValidPrice;
+import pl.akademiaqa.bos.validators.ValidTrim;
+import pl.akademiaqa.bos.validators.YearMin;
 
 import javax.validation.constraints.*;
 import java.math.BigDecimal;
@@ -8,18 +11,22 @@ import java.util.Set;
 
 @Value
 public class UpdateBookPayload {
-    @NotBlank
+    @NotBlank(message = "can not be empty")
+    @ValidTrim
     String title;
-    @NotEmpty
+    @NotEmpty(message = "can not be empty")
     Set<Long> authors;
-    @NotNull
+    @NotNull(message = "can not be empty")
+    @YearMin(1900)
     Integer year;
-    @NotNull
-    @DecimalMin(value = "1.00")
-    // TODO - BUG 3 - Można edytować książkę z maksymalną ceną większą niż 1000.00, max cena to 100000.00
-    @DecimalMax(value = "100000.00")
+    @NotNull(message = "can not be empty")
+    @DecimalMin(value = "1")
+    // TODO - BUG 4 (PUT /books/:id) - Można edytować książkę z maksymalną ceną większą niż 1000, max cena to 100000
+    @DecimalMax(value = "10000")
+    @ValidPrice
     BigDecimal price;
-    @NotNull
-    @PositiveOrZero
+    @NotNull(message = "can not be empty")
+    @Max(10000)
+    @Min(1)
     Integer available;
 }
