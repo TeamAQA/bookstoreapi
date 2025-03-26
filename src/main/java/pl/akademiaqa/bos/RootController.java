14208a3390c54cfc28141ea298c4b76af3536534
🧉 Bartosz Kita <bartosz.kita@gmail.com>
2025-03-26 12:48:38 +0100

    Create RootController.java

---
+++ src/main/java/pl/akademiaqa/bos/RootController.java
@@ -0,0 +1,20 @@
+package pl.akademiaqa.bos;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RequestMapping("/")
+@RestController
+@RequiredArgsConstructor
+public class RootController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, String> rootJson() {
+        return Map.of("message", "Bookstore API is up and running");
+    }
+}
